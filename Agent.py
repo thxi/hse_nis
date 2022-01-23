928ebb4eafaee2baf6f0cec972822f9c42e53dc5
thxi <thxixl@protonmail.com>
2022-01-23 15:50:22 +0300

    some random shit

--- Agent.py
+++ Agent.py
@@ -1,55 +1,52 @@
 import numpy as np
 
-# can send
-#  market order
-#  limit order
-# the class should have
-# why do we need this?
-# why not only create the market?
-
 m = 10
 delta = 1 / m
 
-# algorithm 1: spread based strategy
+
 class Agent:
-    def __init__(self, b, a, a_1, inventory=0, cash=0):
-        self.b = b
-        self.a = a
+    def __init__(self, b, alpha, a_1, inventory=0, cash=0):
+        # strategy S(b)
+        self.b = b  # spread
+        self.alpha = alpha  # liquidity density
         self.inventory = inventory
         self.cash = cash
 
         self.a_t = a_1
 
-    def make_action(self, p_t, p_next):
+    def value(self, p_current):
+        return self.cash + p_current * self.inventory
+
+    def limit_order(self, p_current, p_next):
         # current_price = p_t
-        if p_t < self.a_t:
+        if p_current < self.a_t:
             old = self.a_t
-            self.a_t = p_t
-            print(f"updating {old=} {self.a_t=}")
-        elif p_t > self.a_t + self.b:
+            self.a_t = p_current
+            # print(f"{self.b=} updating {old=} {self.a_t=}")
+        elif p_current > self.a_t + self.b:
             old = self.a_t
-            self.a_t = p_t - self.b
-            print(f"updating {old=} {self.a_t=}")
+            self.a_t = p_current - self.b
+            # print(f"{self.b=} updating {old=} {self.a_t=}")
         else:
-            print(f"not updating {self.a_t=}")
+            # print(f"{self.b=} not updating {self.a_t=}")
             self.a_t = self.a_t
 
-        # submit 2 limit orders
+        # submit limit order
         # should go through discretization range
         if p_next > self.a_t + self.b:
             price_start = self.a_t + self.b
-            print(price_start, self.a_t, self.b)
+            # print(price_start, self.a_t, self.b)
             price_end = p_next
             prices = np.arange(price_start, price_end + delta, delta)
-            print("selling", prices)
+            # print(f"{self.b=} selling", prices)
             for p in prices:
-                self.inventory -= self.a
-                self.cash += p * self.a
+                self.inventory -= self.alpha
+                self.cash += p * self.alpha
         elif p_next < self.a_t:
             price_start = p_next
             price_end = self.a_t
             prices = np.arange(price_start, price_end + delta, delta)
-            print("buying", prices)
+            # print(f"{self.b=} buying", prices)
             for p in prices:
-                self.inventory += self.a
-                self.cash -= p * self.a
+                self.inventory += self.alpha
+                self.cash -= p * self.alpha
